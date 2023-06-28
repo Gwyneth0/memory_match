f7e8f309ec405c4fd16fa1da9ed0898188336e3e
Gwyneth0 <hieulu8902@gmail.com>
2023-06-28 10:18:13 +0700

    done

--- assets/script/menu/menu.ts
+++ assets/script/menu/menu.ts
@@ -1,39 +1,31 @@
-import { _decorator, AudioSource, Button, Component, director, Node } from 'cc';
+import { _decorator, AudioSource, Button, Component, director, find, Node } from 'cc';
 import { audioSouce } from '../game/audioSouce';
 const { ccclass, property } = _decorator;
 
 @ccclass('menu')
 export class menu extends Component {
 
+    private audioSource: AudioSource;
+
     @property(Button)
     private loadScene: Button;
-
-    private audio: AudioSource;
-
-    @property(Button)
-    private on_music: Button;
-
-    @property(Button)
-    private off_music: Button;
-
+S
     protected onLoad(): void {
         this.loadScene.node.on(Node.EventType.TOUCH_END, this.playGame);
+
+    }
+
+    protected start(): void {
+        this.audioSource = this.node.getComponent(AudioSource);
+
+        let sound = localStorage.getItem('soundOn') ? JSON.parse(localStorage.getItem('soundOn')) : true;
+
+        this.audioSource.volume = sound ? 1 : 0;
+        this.audioSource.play();
     }
 
     protected playGame(): void {
         director.loadScene('game');
     }
-
-    protected btnOnMusic(): void {
-        this.audio.volume = 1;
-        this.on_music.node.active = false;
-        this.off_music.node.active = true;
-    }
-
-    protected btnOffKMusic(): void {
-        this.audio.volume = 0;
-        this.on_music.node.active = true;
-        this.off_music.node.active = false;
-    }    
 }
 
